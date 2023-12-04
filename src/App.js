5d199346673dbf16aed28c8f14d959f35959aefa
Zubair Khan <zubair.k.khan@oracle.com>
2023-12-03 18:42:47 -0600

    Basic layout implementation complete

--- src/App.js
+++ src/App.js
@@ -1,47 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import candidates from './candidates.json';
+
+import ilhanImage from './images/ilhan.jpeg';
+import rashidaImage from './images/rashida.jpeg';
+import summerImage from './images/summer.jpeg';
+import coriImage from './images/cori.jpeg';
+import andreImage from './images/andre.jpeg';
+import alexandriaImage from './images/aoc.jpeg';
 
 function App() {
-  // Dummy data for candidates
-  const candidates = [
-    { id: 1, name: "Candidate 1", bio: "Bio of Candidate 1" },
-    { id: 2, name: "Candidate 2", bio: "Bio of Candidate 2" },
-    { id: 3, name: "Candidate 3", bio: "Bio of Candidate 3" },
-    { id: 4, name: "Candidate 4", bio: "Bio of Candidate 4" },
-    { id: 5, name: "Candidate 5", bio: "Bio of Candidate 5" },
-    { id: 6, name: "Candidate 6", bio: "Bio of Candidate 6" },
-    { id: 7, name: "Candidate 7", bio: "Bio of Candidate 7" },
-    { id: 8, name: "Candidate 8", bio: "Bio of Candidate 8" },
-    // ... more candidates
-  ];
+  // Image mapping
+  const imageMap = {
+    1: ilhanImage,
+    2: rashidaImage,
+    3: summerImage,
+    4: coriImage,
+    5: andreImage,
+    6: alexandriaImage,
+  };
 
   return (
-    <div className="container mt-4">
-      <div className="row">
-        {candidates.map(candidate => (
-          <div key={candidate.id} className="col-lg-4 col-md-6 col-sm-12 mb-4">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">{candidate.name}</h5>
-                <p className="card-text">{candidate.bio}</p>
-                <button className="btn btn-primary">Donate</button>
-              </div>
-            </div>
+    <div className="app-container">
+      {candidates.map(candidate => (
+        <div key={candidate.id} className="candidate-card">
+          <img
+            src={imageMap[candidate.id]}
+            alt={candidate.name}
+            className="candidate-image"
+          />
+          <div className="candidate-content">
+            <h5 className="candidate-title">{candidate.name}</h5>
+            <p className="candidate-bio">{candidate.bio}</p>
+            <button className="donate-button">Donate</button>
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
     </div>
   );
-  /*
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Coming Soon</h1>
-        <p>Stay tuned for more information about our candidates and how you can support them.</p>
-      </header>
-    </div>
-  );
-  */
 }
 
 export default App;
+
